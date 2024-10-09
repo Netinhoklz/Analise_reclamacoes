59b46844a94f64fd1b567baac9b8ed5fb784eb22
Netinhoklz <91354525+Netinhoklz@users.noreply.github.com>
2024-10-08 23:52:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,116 +2,146 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import numpy as np
-import os
-
-# Inicializar variáveis padrão para os filtros
-estado_selecionado = None
-status_selecionado = 'Todos os status'
-min_texto = 0
-max_texto = 5000
 
 # Crie três abas no Streamlit
 tab1, tab2, tab3 = st.tabs(["Hapvida", "Nagem", "Ibyte"])
-# Filtros
-data = pd.read_csv('pasta_dados/hapvida.csv')
-estado_selecionado = st.sidebar.selectbox('Selecione o estado:', data['Estado'].unique())
+data = pd.read_csv(f'pasta_dados//hapvida.csv')
+estado_selecionado = st.sidebar.selectbox(
+    'Selecione o estado:',
+    data['Estado'].unique()
+)
 array_status = np.insert(data['STATUS'].unique(), 0, 'Todos os status')
-status_selecionado = st.sidebar.selectbox('Selecione o Status:', array_status)
+status_selecionado = st.sidebar.selectbox(
+    'Selecione o Status:',
+    array_status
+)
 min_texto = st.sidebar.slider("Tamanho mínimo do texto (caracteres)", min_value=0, max_value=5000, value=0)
 max_texto = st.sidebar.slider("Tamanho máximo do texto (caracteres)", min_value=0, max_value=5000, value=5000)
-
-
-# Conteúdo da Aba 1 - Hapvida
+# Conteúdo da Aba 1
 with tab1:
-    # Verificar e carregar os dados da aba 1
-    data_path = 'pasta_dados/hapvida.csv'
-    if not os.path.exists(data_path):
-        st.error(f"Arquivo {data_path} não encontrado.")
+    data = pd.read_csv(f'pasta_dados//hapvida.csv')
+
+    # Convertendo a coluna 'TEMPO' para datetime, se aplicável
+    #data['TEMPO'] = pd.to_datetime(data['TEMPO'])
+
+
+    st.title("Painel de Reclamações - Análise de Dados")
+
+
+    if status_selecionado == 'Todos os status':
+        reclamacao_por_estado = data.loc[(data['Estado'] == estado_selecionado)].copy()
+        reclamacao_por_estado['um'] = 1
+        reclamacao_por_estado['Total_casos'] = reclamacao_por_estado['um'].cumsum()
+        frequencia_tipo_status = reclamacao_por_estado.loc[reclamacao_por_estado.STATUS == status_selecionado]
     else:
-        data = pd.read_csv(data_path)
-        st.title("Painel de Reclamações - Análise de Dados (Hapvida)")
+        reclamacao_por_estado = data.loc[(data['Estado'] == estado_selecionado) &(data['STATUS'] == status_selecionado)].copy()
+        reclamacao_por_estado['um'] = 1
+        reclamacao_por_estado['Total_casos'] = reclamacao_por_estado['um'].cumsum()
+        frequencia_tipo_status = reclamacao_por_estado.loc[reclamacao_por_estado.STATUS == status_selecionado]
+    tamanho_texto = reclamacao_por_estado.loc[(reclamacao_por_estado.DESCRICAO_tamanho >= min_texto) & (reclamacao_por_estado.DESCRICAO_tamanho <= max_texto)]
+    # Série temporal
+    fig_serie_temporal = px.line(reclamacao_por_estado,width= 500, height= 400, x = 'TEMPO', y = 'Total_casos', title = 'Série temporal do número de reclamaçãos diárias', labels = {'TEMPO':'Dias', 'CASOS':'Reclamações diárias'})
+    st.plotly_chart(fig_serie_temporal)
 
+    # Gráfico de barras
+    fig_status = px.bar(reclamacao_por_estado,width= 500, height= 600, x='STATUS', title = 'Ocorrência do Status', labels = {'STATUS':'Status', 'count' : 'Frequência'})
+    st.plotly_chart(fig_status)
 
+    # histograma
+    fig_histogram = px.histogram(tamanho_texto, width= 500, height= 400, x='DESCRICAO_tamanho', title = 'Distribuição do tamanho dos textos', color = 'STATUS', labels = {'DESCRICAO_tamanho':'Tamanho do texto', 'count':'Frequência'})
+    fig_histogram.update_layout(
+        xaxis_title='Tamanho do Texto',  # Rótulo do eixo x
+        yaxis_title='Frequência'  # Rótulo do eixo y
+    )
+    st.plotly_chart(fig_histogram)
 
-        # Aplicar Filtros
-        if status_selecionado == 'Todos os status':
-            reclamacao_por_estado = data[data['Estado'] == estado_selecionado].copy()
-        else:
-            reclamacao_por_estado = data[(data['Estado'] == estado_selecionado) & (data['STATUS'] == status_selecionado)].copy()
-
-        tamanho_texto = reclamacao_por_estado[(reclamacao_por_estado['DESCRICAO_tamanho'] >= min_texto) &
-                                              (reclamacao_por_estado['DESCRICAO_tamanho'] <= max_texto)]
-
-        # Gráficos com Plotly
-        fig_serie_temporal = px.line(reclamacao_por_estado, x='TEMPO', y='CASOS', title='Série Temporal do Número de Reclamações Diárias')
-        st.plotly_chart(fig_serie_temporal)
-
-        fig_status = px.bar(reclamacao_por_estado, x='STATUS', y='CASOS', title='Ocorrência do Status')
-        st.plotly_chart(fig_status)
-
-        fig_histogram = px.histogram(tamanho_texto, x='DESCRICAO_tamanho', color='STATUS', title='Distribuição do Tamanho dos Textos')
-        fig_histogram.update_layout(xaxis_title='Tamanho do Texto', yaxis_title='Frequência')
-        st.plotly_chart(fig_histogram)
-
-# Conteúdo da Aba 2 - Nagem
+# Conteúdo da Aba 2
 with tab2:
-    # Verificar e carregar os dados da aba 2
-    data_path = 'pasta_dados/nagem.csv'
-    if not os.path.exists(data_path):
-        st.error(f"Arquivo {data_path} não encontrado.")
+    data = pd.read_csv(f'pasta_dados//nagem.csv')
+
+    # Convertendo a coluna 'TEMPO' para datetime, se aplicável
+    # data['TEMPO'] = pd.to_datetime(data['TEMPO'])
+
+    st.title("Painel de Reclamações - Análise de Dados")
+
+
+    if status_selecionado == 'Todos os status':
+        reclamacao_por_estado = data.loc[(data['Estado'] == estado_selecionado)].copy()
+        reclamacao_por_estado['um'] = 1
+        reclamacao_por_estado['Total_casos'] = reclamacao_por_estado['um'].cumsum()
+        frequencia_tipo_status = reclamacao_por_estado.loc[reclamacao_por_estado.STATUS == status_selecionado]
     else:
-        data = pd.read_csv(data_path)
-        st.title("Painel de Reclamações - Análise de Dados (Nagem)")
+        reclamacao_por_estado = data.loc[
+            (data['Estado'] == estado_selecionado) & (data['STATUS'] == status_selecionado)].copy()
+        reclamacao_por_estado['um'] = 1
+        reclamacao_por_estado['Total_casos'] = reclamacao_por_estado['um'].cumsum()
+        frequencia_tipo_status = reclamacao_por_estado.loc[reclamacao_por_estado.STATUS == status_selecionado]
+    tamanho_texto = reclamacao_por_estado.loc[(reclamacao_por_estado.DESCRICAO_tamanho >= min_texto) & (
+                reclamacao_por_estado.DESCRICAO_tamanho <= max_texto)]
+    # Série temporal
+    fig_serie_temporal = px.line(reclamacao_por_estado, width=500, height=400, x='TEMPO', y='Total_casos',
+                                 title='Série temporal do número de reclamaçãos diárias',
+                                 labels={'TEMPO': 'Dias', 'CASOS': 'Reclamações diárias'})
+    st.plotly_chart(fig_serie_temporal)
 
-        # Aplicar Filtros
-        if status_selecionado == 'Todos os status':
-            reclamacao_por_estado = data[data['Estado'] == estado_selecionado].copy()
-        else:
-            reclamacao_por_estado = data[(data['Estado'] == estado_selecionado) & (data['STATUS'] == status_selecionado)].copy()
+    # Gráfico de barras
+    fig_status = px.bar(reclamacao_por_estado, width=500, height=600, x='STATUS', title='Ocorrência do Status',
+                        labels={'STATUS': 'Status', 'count': 'Frequência'})
+    st.plotly_chart(fig_status)
 
-        tamanho_texto = reclamacao_por_estado[(reclamacao_por_estado['DESCRICAO_tamanho'] >= min_texto) &
-                                              (reclamacao_por_estado['DESCRICAO_tamanho'] <= max_texto)]
+    # histograma
+    fig_histogram = px.histogram(tamanho_texto, width=500, height=400, x='DESCRICAO_tamanho',
+                                 title='Distribuição do tamanho dos textos', color='STATUS',
+                                 labels={'DESCRICAO_tamanho': 'Tamanho do texto', 'count': 'Frequência'})
+    fig_histogram.update_layout(
+        xaxis_title='Tamanho do Texto',  # Rótulo do eixo x
+        yaxis_title='Frequência'  # Rótulo do eixo y
+    )
+    st.plotly_chart(fig_histogram)
 
-        # Gráficos com Plotly
-        fig_serie_temporal = px.line(reclamacao_por_estado, x='TEMPO', y='CASOS', title='Série Temporal do Número de Reclamações Diárias')
-        st.plotly_chart(fig_serie_temporal)
-
-        fig_status = px.bar(reclamacao_por_estado, x='STATUS', y='CASOS', title='Ocorrência do Status')
-        st.plotly_chart(fig_status)
-
-        fig_histogram = px.histogram(tamanho_texto, x='DESCRICAO_tamanho', color='STATUS', title='Distribuição do Tamanho dos Textos')
-        fig_histogram.update_layout(xaxis_title='Tamanho do Texto', yaxis_title='Frequência')
-        st.plotly_chart(fig_histogram)
-
-# Conteúdo da Aba 3 - Ibyte
+# Conteúdo da Aba 3
 with tab3:
-    # Verificar e carregar os dados da aba 3
-    data_path = 'pasta_dados/ibyte.csv'
-    if not os.path.exists(data_path):
-        st.error(f"Arquivo {data_path} não encontrado.")
+    data = pd.read_csv(f'pasta_dados//ibyte.csv')
+
+    # Convertendo a coluna 'TEMPO' para datetime, se aplicável
+    # data['TEMPO'] = pd.to_datetime(data['TEMPO'])
+
+    st.title("Painel de Reclamações - Análise de Dados")
+
+
+    if status_selecionado == 'Todos os status':
+        reclamacao_por_estado = data.loc[(data['Estado'] == estado_selecionado)].copy()
+        reclamacao_por_estado['um'] = 1
+        reclamacao_por_estado['Total_casos'] = reclamacao_por_estado['um'].cumsum()
+        frequencia_tipo_status = reclamacao_por_estado.loc[reclamacao_por_estado.STATUS == status_selecionado]
     else:
-        data = pd.read_csv(data_path)
-        st.title("Painel de Reclamações - Análise de Dados (Ibyte)")
+        reclamacao_por_estado = data.loc[
+            (data['Estado'] == estado_selecionado) & (data['STATUS'] == status_selecionado)].copy()
+        reclamacao_por_estado['um'] = 1
+        reclamacao_por_estado['Total_casos'] = reclamacao_por_estado['um'].cumsum()
+        frequencia_tipo_status = reclamacao_por_estado.loc[reclamacao_por_estado.STATUS == status_selecionado]
+    tamanho_texto = reclamacao_por_estado.loc[(reclamacao_por_estado.DESCRICAO_tamanho >= min_texto) & (
+                reclamacao_por_estado.DESCRICAO_tamanho <= max_texto)]
+    # Série temporal
+    fig_serie_temporal = px.line(reclamacao_por_estado, width=500, height=400, x='TEMPO', y='Total_casos',
+                                 title='Série temporal do número de reclamaçãos diárias',
+                                 labels={'TEMPO': 'Dias', 'CASOS': 'Reclamações diárias'})
+    st.plotly_chart(fig_serie_temporal)
 
-        # Aplicar Filtros
-        if status_selecionado == 'Todos os status':
-            reclamacao_por_estado = data[data['Estado'] == estado_selecionado].copy()
-        else:
-            reclamacao_por_estado = data[(data['Estado'] == estado_selecionado) & (data['STATUS'] == status_selecionado)].copy()
+    # Gráfico de barras
+    fig_status = px.bar(reclamacao_por_estado, width=500, height=600, x='STATUS', title='Ocorrência do Status',
+                        labels={'STATUS': 'Status', 'count': 'Frequência'})
+    st.plotly_chart(fig_status)
 
-        tamanho_texto = reclamacao_por_estado[(reclamacao_por_estado['DESCRICAO_tamanho'] >= min_texto) &
-                                              (reclamacao_por_estado['DESCRICAO_tamanho'] <= max_texto)]
-
-        # Gráficos com Plotly
-        fig_serie_temporal = px.line(reclamacao_por_estado, x='TEMPO', y='CASOS', title='Série Temporal do Número de Reclamações Diárias')
-        st.plotly_chart(fig_serie_temporal)
-
-        fig_status = px.bar(reclamacao_por_estado, x='STATUS', y='CASOS', title='Ocorrência do Status')
-        st.plotly_chart(fig_status)
-
-        fig_histogram_ = px.histogram(tamanho_texto, x='DESCRICAO_tamanho', color='STATUS', title='Distribuição do Tamanho dos Textos')
-        fig_histogram_.update_layout(xaxis_title='Tamanho do Texto', yaxis_title='Frequência')
-        st.plotly_chart(fig_histogram_)
+    # histograma
+    fig_histogram = px.histogram(tamanho_texto, width=500, height=400, x='DESCRICAO_tamanho',
+                                 title='Distribuição do tamanho dos textos', color='STATUS',
+                                 labels={'DESCRICAO_tamanho': 'Tamanho do texto', 'count': 'Frequência'})
+    fig_histogram.update_layout(
+        xaxis_title='Tamanho do Texto',  # Rótulo do eixo x
+        yaxis_title='Frequência'  # Rótulo do eixo y
+    )
+    st.plotly_chart(fig_histogram)
 
 
 
